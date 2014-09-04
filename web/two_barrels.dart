f08a0330d0f37eb6ba932b1a9169ea5584ba97f9
Joris van de Donk <jorisvddonk@gmail.com>
2014-09-04 20:43:14 +0200

    simplified shaders by removing some cruft / functionality

--- web/two_barrels.dart
+++ web/two_barrels.dart
@@ -106,47 +106,35 @@ class Lesson07 {
     attribute vec3 aVertexPosition;
     attribute vec3 aVertexNormal;
     attribute vec2 aTextureCoord;
+
+    varying vec4 vPosition;
+    varying vec3 vNormal;
+    varying vec2 vTextureCoord;
   
     uniform mat4 uMVMatrix;
     uniform mat4 uPMatrix;
     uniform mat3 uNMatrix;
-  
-    uniform vec3 uAmbientColor;
-  
-    uniform vec3 uLightingDirection;
-    uniform vec3 uDirectionalColor;
-  
-    uniform bool uUseLighting;
-  
-    varying vec2 vTextureCoord;
-    varying vec3 vLightWeighting;
-  
+    
     void main(void) {
-      gl_Position = uPMatrix * uMVMatrix * vec4(aVertexPosition, 1.0);
+      vPosition = uMVMatrix * vec4(aVertexPosition, 1.0);
+      gl_Position = uPMatrix * vPosition;
       vTextureCoord = aTextureCoord;
-  
-      if (!uUseLighting) {
-        vLightWeighting = vec3(1.0, 1.0, 1.0);
-      } else {
-        vec3 transformedNormal = uNMatrix * aVertexNormal;
-        float directionalLightWeighting = max(dot(transformedNormal, uLightingDirection), 0.0);
-        vLightWeighting = uAmbientColor + uDirectionalColor * directionalLightWeighting;
-      }
-    }""";
+      vNormal = uNMatrix * aVertexNormal;
+    }
+    """;
 
     // fragment shader source code. uColor is our variable that we'll
     // use to animate color
     String fsSource = """
     precision mediump float;
-    
+
     varying vec2 vTextureCoord;
-    varying vec3 vLightWeighting;
-    
     uniform sampler2D uSampler;
     
-    void main(void) {
-       vec4 textureColor = texture2D(uSampler, vec2(vTextureCoord.s, vTextureCoord.t));
-       gl_FragColor = vec4(textureColor.rgb * vLightWeighting, textureColor.a);
+    void main(void) { 
+      vec4 fragmentColor;
+      fragmentColor = texture2D(uSampler, vec2(vTextureCoord.s, vTextureCoord.t));
+      gl_FragColor = vec4(fragmentColor.rgb, fragmentColor.a);
     }
     """;
 
